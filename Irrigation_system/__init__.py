7b95587858e3d2088070505a4fd28eb39fbf3131
Muhammad <nadeem.haider34603@gmail.com>
2021-12-17 21:05:11 +0500

    Removes email password

--- Irrigation_system/__init__.py
+++ Irrigation_system/__init__.py
@@ -16,10 +16,8 @@ login_manager.login_message_category = 'info'
 app.config['MAIL_SERVER'] = 'smtp.googlemail.com'
 app.config['MAIL_PORT'] = 587
 app.config['MAIL_USE_TLS'] = True
-# app.config['MAIL_USERNAME'] = os.environ.get('EMAIL_USER')
-# app.config['MAIL_PASSWORD'] = os.environ.get('EMAIL_PASS')
-app.config['MAIL_USERNAME'] = 'nadeem.haider34603@gmail.com'
-app.config['MAIL_PASSWORD'] = 'googoandmoosi'
+app.config['MAIL_USERNAME'] = os.environ.get('EMAIL_USER')
+app.config['MAIL_PASSWORD'] = os.environ.get('EMAIL_PASS')
 mail = Mail(app)
 
 from Irrigation_system import routes
